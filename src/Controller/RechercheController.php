39d93c9341676411e88bb656cd77cb5e605a1f97
Alexandre <126659908+Alexandre0803@users.noreply.github.com>
2023-12-12 16:49:19 +0100

    Update RechercheController.php

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -1,18 +1,20 @@
-
+// src/Controller/RechercheController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ChaineRepository;
 
 class RechercheController extends AbstractController
 {
     /**
      * @Route("/recherche", name="recherche")
      */
-    public function index(): Response
+    public function index(ChaineRepository $chaineRepository): Response
     {
-        // Logique pour effectuer la recherche et afficher les résultats
+        // Récupérer la liste des chaînes (ou effectuer la recherche en fonction des paramètres)
+
         return $this->render('recherche/index.html.twig');
     }
 }
