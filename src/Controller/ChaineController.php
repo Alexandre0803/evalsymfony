8e41f0615b3e765a558437b27945c45a0da6e538
Alexandre <126659908+Alexandre0803@users.noreply.github.com>
2023-12-12 16:49:04 +0100

    Update ChaineController.php

--- src/Controller/ChaineController.php
+++ src/Controller/ChaineController.php
@@ -1,19 +1,26 @@
-
+// src/Controller/ChaineController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ChaineRepository;
+use App\Repository\VideoRepository;
 
 class ChaineController extends AbstractController
 {
     /**
      * @Route("/chaine/{id}", name="chaine")
      */
-    public function index($id): Response
+    public function index($id, ChaineRepository $chaineRepository, VideoRepository $videoRepository): Response
     {
-        // Logique pour récupérer et afficher les vidéos de la chaine avec l'id donné triées par date de publication
-        return $this->render('chaine/index.html.twig');
+        // Récupérer la chaîne spécifique depuis la base de données
+        $chaine = $chaineRepository->find($id);
+
+        // Récupérer les vidéos de la chaîne triées par date de publication
+        $videos = $videoRepository->findBy(['chaine' => $chaine], ['datePublication' => 'ASC']);
+
+        return $this->render('chaine/index.html.twig', ['chaine' => $chaine, 'videos' => $videos]);
     }
 
     // Autres actions pour /chaine/ajouter et /chaine/editer/id
