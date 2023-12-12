9add0ae45d583e0d32add2de6b96bdec4904c895
Alexandre <126659908+Alexandre0803@users.noreply.github.com>
2023-12-12 16:48:19 +0100

    Update AccueilController.php

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,18 +1,21 @@
-
+// src/Controller/AccueilController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ChaineRepository;
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/", name="accueil")
      */
-    public function index(): Response
+    public function index(ChaineRepository $chaineRepository): Response
     {
-        // Logique pour récupérer les chaines et les afficher
-        return $this->render('accueil/index.html.twig');
+        // Récupérer la liste des chaînes depuis la base de données
+        $chaines = $chaineRepository->findAll();
+
+        return $this->render('accueil/index.html.twig', ['chaines' => $chaines]);
     }
 }
